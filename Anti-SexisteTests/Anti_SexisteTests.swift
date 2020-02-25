d9d5b18e128627c2db277ea8b01ee23982be05c4
Debatent <alexandreadrien@orange.fr>
2020-02-25 16:49:19 +0100

    First commit

---
+++ Anti-SexisteTests/Anti_SexisteTests.swift
@@ -0,0 +1,34 @@
+//
+//  Anti_SexisteTests.swift
+//  Anti-SexisteTests
+//
+//  Created by user165109 on 25/02/2020.
+//  Copyright © 2020 user165109. All rights reserved.
+//
+
+import XCTest
+@testable import Anti_Sexiste
+
+class Anti_SexisteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
